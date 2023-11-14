652ad450e34b71d89eabe6f2794f2fc786a53751
starlitskies <majikx.prod0@gmail.com>
2023-11-14 11:33:29 +0500

    feat

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -3,48 +3,53 @@
     <div class="main__body">
       <div class="main__container">
         <div class="settings">
-          <span>
-            <label for="showAlbumArt">Album art</label>
-            <InputSwitch v-model="settings.showAlbumArt" aria-labelledby="showAlbumArt" />
-          </span>
-          <span>
-            <label for="showArtist">Artist name</label>
-            <InputSwitch v-model="settings.showArtist" aria-labelledby="showArtist" />
-          </span>
-          <span>
-            <label for="showBackground">Show background</label>
-            <InputSwitch v-model="settings.showBackground" aria-labelledby="showBackground" />
-          </span>
-          <span>
-            <label for="showBackground">Change Background color</label>
-            <ColorPicker v-model="settings.colorBG" />
-          </span>
-          <span>
-            <label for="showSpotifyLogo">Show Spotify logo</label>
-            <InputSwitch v-model="settings.showSpotifyLogo" aria-labelledby="showSpotifyLogo" />
-          </span>
-          <div class="preview">
-            <div class="preview__content">
-              <Overlay
-                class="widget"
-                :showAlbumArt="settings.showAlbumArt"
-                :showArtist="settings.showArtist"
-                :showBackground="settings.showBackground"
-                :colorBG="settings.colorBG"
-                :showSpotifyLogo="settings.showSpotifyLogo"
-                :fakeTitle="'Blinding Lights'"
-                :fakeArtist="'The Weeknd'"
-                :fakeAlbumArt="'/src/assets/fakeAlbumArt.webp'"
-                :accessToken="accessToken"
-              />
-            </div>
-            <div class="preview__background">
-              <img src="@/assets/preview_bg.webp" alt="" />
-            </div>
+          <div class="settings__content">
+            <h1 class="settings__title">Settings</h1>
+            <span>
+              <label for="showAlbumArt">Album art</label>
+              <InputSwitch v-model="settings.showAlbumArt" aria-labelledby="showAlbumArt" />
+            </span>
+            <span>
+              <label for="showArtist">Artist name</label>
+              <InputSwitch v-model="settings.showArtist" aria-labelledby="showArtist" />
+            </span>
+            <span>
+              <label for="showBackground">Show background</label>
+              <InputSwitch v-model="settings.showBackground" aria-labelledby="showBackground" />
+            </span>
+            <span>
+              <label for="showBackground">Change Background color</label>
+              <ColorPicker v-model="settings.colorBG" />
+            </span>
+            <span>
+              <label for="showSpotifyLogo">Show Spotify logo</label>
+              <InputSwitch v-model="settings.showSpotifyLogo" aria-labelledby="showSpotifyLogo" />
+            </span>
           </div>
-          <div class="settings__input">
-            <InputText id="linkOBS" rows="5" cols="82" class="textarea" readonly v-model="browserSourceUrl" />
-            <Button @click="copyText()" icon="pi pi-copy" aria-label="Filter" />
+          <div class="settings__preview">
+            <div class="preview">
+              <div class="preview__content">
+                <Overlay
+                  class="widget"
+                  :showAlbumArt="settings.showAlbumArt"
+                  :showArtist="settings.showArtist"
+                  :showBackground="settings.showBackground"
+                  :colorBG="settings.colorBG"
+                  :showSpotifyLogo="settings.showSpotifyLogo"
+                  :fakeTitle="'Blinding Lights'"
+                  :fakeArtist="'The Weeknd'"
+                  :fakeAlbumArt="'/src/assets/fakeAlbumArt.webp'"
+                  :accessToken="accessToken"
+                />
+              </div>
+              <div class="preview__background">
+                <img src="@/assets/preview_bg.webp" alt="" />
+              </div>
+            </div>
+            <div class="settings__input">
+              <InputText id="linkOBS" rows="5" cols="82" class="textarea" readonly v-model="browserSourceUrl" />
+              <Button @click="copyText()" icon="pi pi-copy" aria-label="Filter" />
+            </div>
           </div>
         </div>
       </div>
