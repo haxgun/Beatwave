873d75042fffd7e649a9b59ad0fe7e1331e584bf
MAGICX <majikx.prod0@gmail.com>
2024-12-09 02:07:23 +0500

    feat: add router guard

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+function hasAccessToken() {
+  const cookies = document.cookie.split(';').map(c => c.trim());
+  return cookies.some(cookie => cookie.startsWith('access_token='));
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       name: 'index',
       path: '/',
-      component: () => import('@/views/IndexView.vue')
+      component: () => import('@/views/IndexView.vue'),
+      beforeEnter: (to, from, next) => hasAccessToken() ? next({ name: 'setttings' }) : next(),
     },
 
     {
       name: 'settings',
       path: '/settings',
-      component: () => import('@/views/SettingsView.vue')
+      component: () => import('@/views/SettingsView.vue'),
+      beforeEnter: (to, from, next) => hasAccessToken() ? next() : next({ name: 'index' }),
     },
 
     {
